e51b6b610b8c5e358224c8a20c2299e333078d04
vDawgg <veit.laule@gmail.com>
2022-06-07 16:03:59 +0200

    Moved db setup to separate file, included Christians area query with some additions/reconfigurations, added prototype for querying the relevant area.

--- src/app/testfirstcode.py
+++ src/app/testfirstcode.py
@@ -1,53 +1,9 @@
 #!/usr/bin/python
-import psycopg2
-from fastapi import FastAPI
+from pprint import pprint
+
 import requests as req
-
-
-def connect():
-    """ Connect to the PostgreSQL database server """
-    # from some website
-    conn = None
-    try:
-
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(
-            host="localhost",
-            database="simra",
-            user="simra",
-            password="simra12345simra")
-
-        # create a cursor
-        cur = conn.cursor()
-
-        # execute a statement
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
-
-        # display the PostgreSQL database server version
-        db_version = cur.fetchone()
-        print(db_version)
-
-        return conn, cur
-
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-
-
-def close_connection(conn, cur):
-    # from some website. Originally in connect() function
-    try:
-        # close the communication with the PostgreSQL
-        cur.close()
-
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
+import db
+import area
 
 
 def execute_queries(cur, osm_id):
@@ -57,7 +13,25 @@ def execute_queries(cur, osm_id):
     print(cur.fetchall())
 
 
-def testApi():
+def query_area(north, east, south, west):
+    if north > south:
+        if east > west:
+            body = {
+                "ne": str(north)+","+str(east),
+                "sw": str(north-0.001)+","+str(east-0.001)
+            }
+            response = req.post("http://127.0.0.1:8000/area", json=body)
+
+            return response.json(), north, east-0.001
+
+        else:
+            return None, north-0.001, None
+
+    else:
+        return None, None, None
+
+
+def test_api():
     # Structure of the json we get atm:
     # Json dict contains one key "features"
     # "Features" values is a list with one dict for every infrastructure type
@@ -65,25 +39,38 @@ def testApi():
     # a list with a dict for every street that corresponds to this type
     # This dict has the osm-id under the key "id"
 
-    request = req.get("http://127.0.0.1:8000")
-    requested_data = request.json()["features"]
-
     osm_ids_per_infrastructure = {}
-    for infrastructure_dict in requested_data:
 
-        for infra_type, streets in infrastructure_dict.items():
-            osm_ids = []
-            for street_data in streets:
-                osm_ids.append(street_data["id"])
-            osm_ids_per_infrastructure[infra_type] = osm_ids
+    north, east, south, west = area.find_borders()
+    n, e = north, east
+
+    while True:
+        requested_data, n, e = query_area(n, e, south, west)
+
+        if e is None:
+            e = east
+            continue
+
+        if n is None:
+            break
+
+        print(requested_data)
+
+        for infrastructure_dict in requested_data["features"]:
+
+            for infra_type, streets in infrastructure_dict.items():
+                osm_ids = []
+                for street_data in streets:
+                    osm_ids.append(street_data["id"])
+                    osm_ids_per_infrastructure[infra_type] = osm_ids
 
     return osm_ids_per_infrastructure
 
 
 if __name__ == '__main__':
-    conn, cur = connect()
+    conn, cur = db.connect()
 
-    osm_ids_per_infrastructure = testApi()
+    osm_ids_per_infrastructure = test_api()
 
     for infra, osm_ids in osm_ids_per_infrastructure.items():
         print(f"WORKING ON ALL OSM_IDS WITH INFRASTRUCTURE TYPE {infra}. THERE ARE {len(osm_ids)} IDS FOR THIS "
@@ -91,5 +78,4 @@ if __name__ == '__main__':
         for osm_id in osm_ids:
             execute_queries(cur, osm_id)
 
-    close_connection(conn, cur)
-
+    db.close_connection(conn, cur)

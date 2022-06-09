4dc8e04122a46763770d281a24a54e908af7b1a8
tobiasengelbrecht <tobias.engelbrecht98@gmail.com>
2022-06-09 15:52:40 +0200

    scores algo updated to work with real count (accumulated count of how often all legs of one infra type are taken)

--- src/app/scores.py
+++ src/app/scores.py
@@ -4,7 +4,7 @@ import math
 
 
 if __name__ == '__main__':
-    counts_by_infra = pd.read_csv("../../counts.csv", index_col=0)
+    counts_by_infra = pd.read_csv("../../test_counts.csv", index_col=0)
 
     # add columns for scores we want to calculate
     #counts_by_infra[' avoided_score'] = pd.Series(np.zeros(len(counts_by_infra.index)), index=counts_by_infra.index)
@@ -12,12 +12,12 @@ if __name__ == '__main__':
     #counts_by_infra[' popularity_score'] = pd.Series(np.zeros(len(counts_by_infra.index)), index=counts_by_infra.index)
     #counts_by_infra[' mixed_popularity_score'] = pd.Series(np.zeros(len(counts_by_infra.index)), index=counts_by_infra.index)
 
-    counts_by_infra[' avoided_score'] = counts_by_infra.loc[:, ' avoided_count'] / counts_by_infra.loc[:, ' leg_count']
-    counts_by_infra[' chosen_score'] = counts_by_infra.loc[:, ' chosen_count'] / counts_by_infra.loc[:, ' leg_count']
-    counts_by_infra[' popularity_score'] = ((1 - counts_by_infra[' avoided_score']) + counts_by_infra[' chosen_score']) / 2
+    counts_by_infra['avoided_score'] = counts_by_infra.loc[:, 'avoided_count'] / counts_by_infra.loc[:, 'count']
+    counts_by_infra['chosen_score'] = counts_by_infra.loc[:, 'chosen_count'] / counts_by_infra.loc[:, 'count']
+    counts_by_infra['popularity_score'] = ((1 - counts_by_infra['avoided_score']) + counts_by_infra['chosen_score']) / 2
 
-    counts_by_infra[' temporary_safety_score'] = 1 - (1 / counts_by_infra[' leg_count']) * (2 * counts_by_infra[' scary_incident_count'] + counts_by_infra[' normal_incident_count'])
-    counts_by_infra[' mixed_popularity_score'] = ((1 - counts_by_infra[' avoided_score']) + counts_by_infra[' chosen_score'] + counts_by_infra[' temporary_safety_score'] * 2) / 4
+    counts_by_infra['temporary_safety_score'] = 1 - (1 / counts_by_infra['count']) * (2 * counts_by_infra['scary_incident_count'] + counts_by_infra['normal_incident_count'])
+    counts_by_infra['mixed_popularity_score'] = ((1 - counts_by_infra['avoided_score']) + counts_by_infra['chosen_score'] + counts_by_infra['temporary_safety_score'] * 2) / 4
 
     #for i in range(len(counts_by_infra.index)):
     #    index_label = counts_by_infra.index[i]
@@ -25,4 +25,4 @@ if __name__ == '__main__':
     #        print()
 
     counts_by_infra.to_csv("../../scores.csv")
-    print(counts_by_infra[[' temporary_safety_score', ' mixed_popularity_score']])
+    print(counts_by_infra[['temporary_safety_score', 'mixed_popularity_score']])

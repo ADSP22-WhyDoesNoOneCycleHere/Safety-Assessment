5eb792732e3ba6d41fc55e6be7a7a0945dce3d5f
Tobias Engelbrecht <tobias.engelbrecht@stud-mail.uni-wuerzburg.de>
2022-06-10 10:16:56 +0200

    csv data and scores for Berlin area. Minor refactoring

--- src/app/get_data.py
+++ src/app/get_data.py
@@ -12,8 +12,14 @@ infra_dict = {}  # Dict to which the different counts are saved
 
 
 def execute_queries(cur, osm_id, infra_type):
+    """
+    Get the important counts by accumulating the counts of each leg of this osm_id from the database
+    :param cur: current database
+    :param osm_id: unique identifier to some part of a street or the complete street
+    :param infra_type: the infrastructure for which we are querying right now
+    :return: saved internally to the global dict
+    """
 
-    # TODO: Look into what counts are actually important for the scores!
     query = f'Select "avoidedCount", "chosenCount", "normalIncidentCount", "scaryIncidentCount", "osmId", "count"' \
             f' from "SimRaAPI_osmwayslegs" where "osmId"={osm_id}'
 
@@ -26,15 +32,17 @@ def execute_queries(cur, osm_id, infra_type):
         infra_dict[infra_type]["chosen_count"] += count[1]
         infra_dict[infra_type]["normal_incident_count"] += count[2]
         infra_dict[infra_type]["scary_incident_count"] += count[3]
+
         # the first time an unchecked id occurs -> add way length to the complete length
         # check for first time
-        tmp_osm_id = count[4]
-       
-        if not tmp_osm_id in analysed_osm_ids:
-            infra_dict[infra_type]["length"] += testlength.get_length2(tmp_osm_id)
-            analysed_osm_ids.append(tmp_osm_id)
+        #tmp_osm_id = count[4]
+        #if not tmp_osm_id in analysed_osm_ids:
+        #    infra_dict[infra_type]["length"] += testlength.get_length2(tmp_osm_id)
+        #    analysed_osm_ids.append(tmp_osm_id)
+        infra_dict[infra_type]["length"] += 0
 
         infra_dict[infra_type]["count"] += count[5]
+        infra_dict[infra_type]["leg_count"] += 1
 
 
 def query_area(north, east, south, west):
@@ -55,7 +63,7 @@ def test():
     return response.json()
 
 
-def test_api():
+def osm_ids_per_infrastructure():
     # Structure of the json we get atm:
     # Json dict contains one key "features"
     # "Features" values is a list with one dict for every infrastructure type
@@ -63,31 +71,31 @@ def test_api():
     # a list with a dict for every street that corresponds to this type
     # This dict has the osm-id under the key "id"
 
-    osm_ids_per_infrastructure = {}
+    infrastructure_osm_ids = {}
 
     north, east, south, west = area.find_borders()
 
     # Uncomment the line below to query the whole relevant area (program takes ages to complete)
-    # requested_data = query_area(north, east, south, west)
+    requested_data = query_area(north, east, south, west)
 
-    requested_data = test()
+    # requested_data = test()
 
     for infrastructure_dict in requested_data["features"]:
         for infra_type, streets in infrastructure_dict.items():
             osm_ids = []
             for street_data in streets:
                 osm_ids.append(street_data["id"])
-                osm_ids_per_infrastructure[infra_type] = osm_ids
+                infrastructure_osm_ids[infra_type] = osm_ids
 
-    return osm_ids_per_infrastructure
+    return infrastructure_osm_ids
 
 
 if __name__ == '__main__':
     conn, cur = db.connect()
 
-    osm_ids_per_infrastructure = test_api()
+    infrastructure_osm_ids = osm_ids_per_infrastructure()
 
-    for infra_type, osm_ids in osm_ids_per_infrastructure.items():
+    for infra_type, osm_ids in infrastructure_osm_ids.items():
 
         infra_types.append(infra_type)
 

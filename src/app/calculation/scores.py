96a73d90d2652fb504bba7be827870d5f5cc7e3f
vDawgg <veit.laule@gmail.com>
2022-06-21 00:03:00 +0000

    Rewrote scores and getdata for computing the scores for individual legs and adding them to the db

--- src/app/calculation/scores.py
+++ src/app/calculation/scores.py
@@ -1,20 +1,43 @@
-import pandas as pd
+def calculate_scores_ways(leg, cur, conn):
+    leg['a_score'] = min(leg['a_count'] / leg['count'], 1)
+    leg['c_score'] = min(leg['c_count'] / leg['count'], 1)
+    leg['p_score'] = ((1 - leg['a_score']) + leg['c_score']) / 2
+
+    # Scary incidents times 4.4 as this is the weight that was calculated for Berlin (see bachelors thesis)
+    # min(x,1) as score can not be higher than 1
+    leg['s_score'] = max(min(1 - (1 / leg['count'] * leg['length']) * (4.4 * leg['scary_incident_count'] + leg['normal_incident_count']), 1), 0)
+    leg['m_p_score'] = ((1 - leg['a_score']) + leg['c_score'] + leg['s_score'] * 2) / 4
+
+    # There should be a smarter way to do this
+    cur.execute(f"select * from leg_scores where id = {leg['id']}")
+
+    if len(cur.fetchall()) == 0:
+        query = f"insert into leg_scores (id, infra_type, a_score, c_score, p_score, s_score, m_p_score)" \
+                f"values(" \
+                f"{leg['id']}, " \
+                f"'{leg['infra_type']}', " \
+                f"{leg['a_score']}, " \
+                f"{leg['c_score']}, " \
+                f"{leg['p_score']}, " \
+                f"{leg['s_score']}, " \
+                f"{leg['m_p_score']});"
+
+        cur.execute(query)
+        conn.commit()
 
 
-if __name__ == '__main__':
-    counts_by_infra = pd.read_csv("../../test.csv", index_col=0)
+def initialize_score_table(cur, conn):
+    query = "create table if not exists leg_scores(" \
+            "id serial primary key," \
+            "infra_type text, " \
+            "a_score numeric, " \
+            "c_score numeric, " \
+            "p_score numeric, " \
+            "s_score numeric, " \
+            "m_p_score numeric);"
 
-    counts_by_infra['avoided_score'] = counts_by_infra['avoided_count'] / counts_by_infra['count']
-    counts_by_infra['chosen_score'] = counts_by_infra['chosen_count'] / counts_by_infra['count']
-    counts_by_infra['popularity_score'] = ((1 - counts_by_infra['avoided_score']) + counts_by_infra['chosen_score']) / 2
+    cur.execute(query)
+    conn.commit()
 
-    counts_by_infra['temporary_safety_score'] = 1 - (1 / counts_by_infra['count']) * (2 * counts_by_infra['scary_incident_count'] + counts_by_infra['normal_incident_count'])
-    counts_by_infra['mixed_popularity_score'] = ((1 - counts_by_infra['avoided_score']) + counts_by_infra['chosen_score'] + counts_by_infra['temporary_safety_score'] * 2) / 4
-
-    #for i in range(len(counts_by_infra.index)):
-    #    index_label = counts_by_infra.index[i]
-    #    if not math.isnan(counts_by_infra.loc[index_label, ' avoided_score']):
-    #        print()
-
-    counts_by_infra.to_csv("../../scores.csv")
-    print(counts_by_infra[['temporary_safety_score', 'mixed_popularity_score']])
+    cur.execute("truncate leg_scores;")
+    conn.commit()
--- src/app/data/get_data.py
+++ src/app/data/get_data.py
@@ -1,79 +1,51 @@
 #!/usr/bin/python
-from pprint import pprint
+import time
 
-import pandas as pd
-import requests as req
+from src.app.data import db
+from src.app.calculation import area
+from src.app.calculation import scores
+from src.app.data.highway import Highway
 
-import db
-from ..calculation import area
-from ..test import testlength
-from .highway import Highway
-
-infra_types = []
-infra_dict = {}  # Dict to which the different counts are saved
+leg = {}  # Dict to which the different counts are saved
 
 
 def execute_queries(cur, osm_id, infra_type):
-    """
-    Get the important counts by accumulating the counts of each leg of this osm_id from the database
-    :param cur: current database
-    :param osm_id: unique identifier to some part of a street or the complete street
-    :param infra_type: the infrastructure for which we are querying right now
-    :return: saved internally to the global dict
-    """
-
-    query = f'Select "avoidedCount", "chosenCount", "normalIncidentCount", "scaryIncidentCount", "osmId", "count"' \
-            f' from "SimRaAPI_osmwayslegs" where "osmId"={osm_id}'
+    query = f'Select "id", "avoidedCount", "chosenCount", "normalIncidentCount", ' \
+            f'"scaryIncidentCount", "count", ST_Length(geom::geography) as length' \
+            f' from "SimRaAPI_osmwayslegs" where "osmId"={osm_id} and count > 0;'
 
     cur.execute(query)
 
-    analysed_osm_ids = []
-
     for count in cur.fetchall():
-        infra_dict[infra_type]["avoided_count"] += count[0]
-        infra_dict[infra_type]["chosen_count"] += count[1]
-        infra_dict[infra_type]["normal_incident_count"] += count[2]
-        infra_dict[infra_type]["scary_incident_count"] += count[3]
+        leg["infra_type"] = infra_type
+        leg["id"] = count[0]
+        leg["a_count"] = count[1]
+        leg["c_count"] = count[2]
+        leg["normal_incident_count"] = count[3]
+        leg["scary_incident_count"] = count[4]
+        leg["count"] = count[5]
+        leg["length"] = count[6]
 
-        # the first time an unchecked id occurs -> add way length to the complete length
-        # check for first time
-        #tmp_osm_id = count[4]
-        #if not tmp_osm_id in analysed_osm_ids:
-        #    infra_dict[infra_type]["length"] += testlength.get_length2(tmp_osm_id)
-        #    analysed_osm_ids.append(tmp_osm_id)
-        infra_dict[infra_type]["length"] += 0
-
-        infra_dict[infra_type]["count"] += count[5]
-        infra_dict[infra_type]["leg_count"] += 1
+        scores.calculate_scores_ways(leg, cur, conn)
 
 
 def query_area(north, east, south, west):
-
     return Highway.query_area(str(south) + "," + str(west), str(north) + "," + str(east))
 
 
 # Use this function when testing new features; query_area() queries ALL of Berlin
 def test():
-
-    return Highway.query_area
+    return Highway.query_area()
 
 
 def osm_ids_per_infrastructure():
-    # Structure of the json we get atm:
-    # Json dict contains one key "features"
-    # "Features" values is a list with one dict for every infrastructure type
-    # Each of these dicts have their identifier as a key (Example key: '[highway = trunk]') and the value of this key is
-    # a list with a dict for every street that corresponds to this type
-    # This dict has the osm-id under the key "id"
-
     infrastructure_osm_ids = {}
 
-    north, east, south, west = area.find_borders()
+    # Uncomment the lines below to query the whole relevant area (program takes ages to complete)
+    # north, east, south, west = area.find_borders()
+    # requested_data = query_area(north, east, south, west)
 
-    # Uncomment the line below to query the whole relevant area (program takes ages to complete)
-    requested_data = query_area(north, east, south, west)
-
-    # requested_data = test()
+    requested_data = test()
 
     for infrastructure_dict in requested_data["features"]:
         for infra_type, streets in infrastructure_dict.items():
@@ -87,31 +59,18 @@ def osm_ids_per_infrastructure():
 
 if __name__ == '__main__':
     conn, cur = db.connect()
+    scores.initialize_score_table(cur, conn)
 
     infrastructure_osm_ids = osm_ids_per_infrastructure()
 
+    start = time.time()
+
     for infra_type, osm_ids in infrastructure_osm_ids.items():
-
-        infra_types.append(infra_type)
-
-        infra_dict[infra_type] = {}
-        infra_dict[infra_type]["avoided_count"] = 0
-        infra_dict[infra_type]["chosen_count"] = 0
-        infra_dict[infra_type]["normal_incident_count"] = 0
-        infra_dict[infra_type]["scary_incident_count"] = 0
-        infra_dict[infra_type]["leg_count"] = 0
-        infra_dict[infra_type]["length"] = 0
-        infra_dict[infra_type]["count"] = 0
-
-        print(f"WORKING ON ALL OSM_IDS WITH INFRASTRUCTURE TYPE {infra_type}. THERE ARE {len(osm_ids)} IDS FOR THIS "
-              f"INFRASTRUCTURE TYPE")
-
         for osm_id in osm_ids:
             execute_queries(cur, osm_id, infra_type)
 
-        print(infra_dict[infra_type])
+    end = time.time()
 
-    df = pd.DataFrame.from_dict(infra_dict, orient="index")
-    df.to_csv("../../test.csv")
+    print(f'Time taken python: {end - start}')
 
     db.close_connection(conn, cur)

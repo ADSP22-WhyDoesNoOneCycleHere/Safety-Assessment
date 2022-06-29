4a8b1590343a93f7ff72afba9f8c744739d55639
dittmanndennis <dennis1dittmann@gmail.com>
2022-06-29 19:57:53 +0200

    bug fix

--- src/app/main.py
+++ src/app/main.py
@@ -1,8 +1,8 @@
 import json
 
 if __name__ == "__main__":
-    with open("areas.json") as f:
-        areas = json.loads(f)
+    with open("./src/app/areas.json") as f: # for docker: ./app/areas.json
+        areas = json.load(f)
         for area in areas["areas"]:
             print("sw", area[0])
             print("ne", area[1])

b43fce5af364e8aa0dcebdc212f45edc0fc847b4
dittmanndennis <dennis1dittmann@gmail.com>
2022-06-30 11:08:54 +0200

    working now

--- src/app/data/highway.py
+++ src/app/data/highway.py
@@ -1,55 +1,53 @@
 from multiprocessing import Pool, cpu_count
 import overpass
 
-api = overpass.API(endpoint="https://vm3.mcc.tu-berlin.de:8088/api/interpreter", timeout=90)
+api = overpass.API(endpoint="https://lz4.overpass-api.de/api/interpreter", timeout=90)
 
-#highway = [
-#            "[highway = trunk]",
-#            "[highway = primary]",
-#            "[highway = secondary]",
-#            "[highway = tertiary]",
-#            "[highway = unclassified]",
-#            "[highway = residential]", "[highway = residential][~'^parking:.*$'~'.'][!cycleway]",
-#            "[highway = motorway_link]",
-#            "[highway = trunk_link]",
-#            "[highway = primary_link]",
-#            "[highway = secondary_link]",
-#            "[highway = tertiary_link]",
-#            "[highway = living_street]",
-#            "[highway = service]",
-#            "[highway = pedestrian]",
-#            "[highway = track]",
-#            "[highway = bus_guideway]",
-#            "[highway = escape]",
-#            "[highway = road]",
-#            "[highway = busway]",
-#            "[highway = footway][bicycle = yes]", "[highway = footway][bicycle = no]", "[highway = footway]",
-#            "[highway = bridleway]",
-#            "[highway = steps]",
-#            "[highway = corridor]",
-#            "[highway = path]",
-#            "[footway = sidewalk][bicycle = yes]", "[footway = sidewalk][bicycle = no]", "[footway = sidewalk]",
-#            "[footway = crossing][bicycle = yes]", "[footway = crossing][bicycle = no]", "[footway = crossing]",
-#            "[highway = cycleway]",
-#            [ "[cycleway = lane]", "['cycleway:left' = lane]", "['cycleway:right' = lane]", "['cycleway:both' = lane]" ],
-#            [ "[cycleway = opposite]", "['cycleway:left' = opposite]", "['cycleway:right' = opposite]", "['cycleway:both' = opposite]" ],
-#            [ "[cycleway = opposite_lane]", "['cycleway:left' = opposite_lane]", "['cycleway:right' = opposite_lane]", "['cycleway:both' = opposite_lane]" ],
-#            [ "[cycleway = separate]", "['cycleway:left' = separate]", "['cycleway:right' = separate]", "['cycleway:both' = separate]", "[cycleway = track]", "['cycleway:left' = track]", "['cycleway:right' = track]", "['cycleway:both' = track]" ],
-#            [ "[cycleway = opposite_track]", "['cycleway:left' = opposite_track]", "['cycleway:right' = opposite_track]", "['cycleway:both' = opposite_track]" ],
-#            [ "[cycleway = share_busway]", "['cycleway:left' = share_busway]", "['cycleway:right' = share_busway]", "['cycleway:both' = share_busway]" ],
-#            [ "[cycleway = opposite_share_busway]", "['cycleway:left' = opposite_share_busway]", "['cycleway:right' = opposite_share_busway]", "['cycleway:both' = opposite_share_busway]" ],
-#            [ "[cycleway = shared_lane]", "['cycleway:left' = shared_lane]", "['cycleway:right' = shared_lane]", "['cycleway:both' = shared_lane]" ],
-#            "[busway = lane]",
-#            "[highway = construction]"
-#        ]
-
-highway = [ "[highway = trunk]" ]
+highway = [
+            "[highway = trunk]",
+            "[highway = primary]",
+            "[highway = secondary]",
+            "[highway = tertiary]",
+            "[highway = unclassified]",
+            "[highway = residential]", "[highway = residential][~'^parking:.*$'~'.'][!cycleway]",
+            "[highway = motorway_link]",
+            "[highway = trunk_link]",
+            "[highway = primary_link]",
+            "[highway = secondary_link]",
+            "[highway = tertiary_link]",
+            "[highway = living_street]",
+            "[highway = service]",
+            "[highway = pedestrian]",
+            "[highway = track]",
+            "[highway = bus_guideway]",
+            "[highway = escape]",
+            "[highway = road]",
+            "[highway = busway]",
+            "[highway = footway][bicycle = yes]", "[highway = footway][bicycle = no]", "[highway = footway]",
+            "[highway = bridleway]",
+            "[highway = steps]",
+            "[highway = corridor]",
+            "[highway = path]",
+            "[footway = sidewalk][bicycle = yes]", "[footway = sidewalk][bicycle = no]", "[footway = sidewalk]",
+            "[footway = crossing][bicycle = yes]", "[footway = crossing][bicycle = no]", "[footway = crossing]",
+            "[highway = cycleway]",
+            [ "[cycleway = lane]", "['cycleway:left' = lane]", "['cycleway:right' = lane]", "['cycleway:both' = lane]" ],
+            [ "[cycleway = opposite]", "['cycleway:left' = opposite]", "['cycleway:right' = opposite]", "['cycleway:both' = opposite]" ],
+            [ "[cycleway = opposite_lane]", "['cycleway:left' = opposite_lane]", "['cycleway:right' = opposite_lane]", "['cycleway:both' = opposite_lane]" ],
+            [ "[cycleway = separate]", "['cycleway:left' = separate]", "['cycleway:right' = separate]", "['cycleway:both' = separate]", "[cycleway = track]", "['cycleway:left' = track]", "['cycleway:right' = track]", "['cycleway:both' = track]" ],
+            [ "[cycleway = opposite_track]", "['cycleway:left' = opposite_track]", "['cycleway:right' = opposite_track]", "['cycleway:both' = opposite_track]" ],
+            [ "[cycleway = share_busway]", "['cycleway:left' = share_busway]", "['cycleway:right' = share_busway]", "['cycleway:both' = share_busway]" ],
+            [ "[cycleway = opposite_share_busway]", "['cycleway:left' = opposite_share_busway]", "['cycleway:right' = opposite_share_busway]", "['cycleway:both' = opposite_share_busway]" ],
+            [ "[cycleway = shared_lane]", "['cycleway:left' = shared_lane]", "['cycleway:right' = shared_lane]", "['cycleway:both' = shared_lane]" ],
+            "[busway = lane]",
+            "[highway = construction]"
+        ]
 
 class Highway:
 
-    def query_area(name = "Berlin"):
+    def query_area(name = "Berlin", sw = "52.28732211531842, 12.91903273460537", ne = "52.69863091076533, 13.86972353817903"):
         pool = Pool(cpu_count())
-        features = pool.map(Highway.queries, [ (hw, name) for hw in highway ])
+        features = pool.map(Highway.queries, [ (hw, name, sw, ne) for hw in highway ])
         
         pool.close()
         pool.join()
@@ -59,14 +57,16 @@ class Highway:
     def queries(args):
         infra_types = args[0]
         name = args[1]
+        sw = args[2]
+        ne = args[3]
         if isinstance(infra_types, list):
             elements = [ ]
             for infra_type in infra_types:
-                query = "area[name = " + name + "]; way" + infra_type + ";"
+                query = "area[name = " + name + "]; way" + infra_type + "(area)(" + sw + "," + ne + ");"
                 elements.append(api.get(query, responseformat="json")["elements"])
             return { infra_types[0]: elements }
         else:
-            res = api.get("area[name = " + name + "]; way" + infra_types + ";", responseformat="json")
+            res = api.get("area[name = " + name + "]; way" + infra_types + "(area)(" + sw + "," + ne + ");", responseformat="json")
             return { infra_types: res["elements"] }
 
 

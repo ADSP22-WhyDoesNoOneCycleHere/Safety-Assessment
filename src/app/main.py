f58a44ee825432bf4d68af3994d8e2068b68b90d
dittmanndennis <dennis1dittmann@gmail.com>
2022-07-07 16:27:55 +0200

    update main

--- src/app/main.py
+++ src/app/main.py
@@ -4,8 +4,4 @@ from data.get_data import main
 
 if __name__ == "__main__":
     main()
-    with open("./src/app/areas.json") as f: # for docker: ./app/areas.json
-        areas = json.load(f)
-        for area in areas["areas"]:
-            print(area)
     

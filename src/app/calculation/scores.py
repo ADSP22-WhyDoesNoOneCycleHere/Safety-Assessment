41bf7fde176df03bb3b6c2f8c25004668817ee53
vDawgg <veit.laule@gmail.com>
2022-08-14 18:01:41 +0200

    Fix division by zero

--- src/app/calculation/scores.py
+++ src/app/calculation/scores.py
@@ -76,15 +76,15 @@ def calculate_scores_infra_types(infra_type, cur, conn):
             f'sum("normalIncidentCount") as incident_count, ' \
             f'round(avg("normalIncidentCount"), 4) as avg_incident_count, ' \
             f'sum("scaryIncidentCount") as scary_incident_count, ' \
-            f'round(avg("scaryIncidentCount" / count), 4) as avg_scary_incident_count, ' \
+            f'round(avg("scaryIncidentCount"), 4) as avg_scary_incident_count, ' \
             f'round(avg("chosenCount"), 4) as avg_c_count, ' \
-            f'round(avg("avoidedCount" / count), 4) as avg_a_count, ' \
+            f'round(avg("avoidedCount"), 4) as avg_a_count, ' \
             f'round(avg(danger_score)) as avg_danger_score, ' \
             f'round(sum("chosenCount")::numeric / (sum("avoidedCount")::numeric + sum("chosenCount")::numeric), 4)::numeric as p_score, ' \
             f'round(greatest(least(1 - (1 / sum(count * (ST_Length(geom::geography)::numeric / 1000))::numeric * (4.4 * sum("scaryIncidentCount")::numeric + sum("normalIncidentCount")::numeric)), 1)::numeric, 0)::numeric, 4)::numeric as s_score ' \
             f'from "SimRaAPI_osmwayslegs" ' \
             f"where '{infra_type}' = any(infra_type) " \
-            f'and count > 0 or "avoidedCount" > 0' \
+            f'and (count > 0 or "avoidedCount" > 0)' \
             f") f;"
 
     cur.execute(query)
